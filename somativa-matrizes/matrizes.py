66ec65a73fed754ec91586a64421fa7fa1252a3a
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2024-06-04 11:44:02 -0300

    somativa

--- somativa-matrizes/matrizes.py
+++ somativa-matrizes/matrizes.py
@@ -1,2 +1,22 @@
 # Integrante - 1: Vinicius da Rosa Padilha
 # Integrante - 2: Murilo Regnier Stange
+
+matrizA = [
+    [3, 4, -2],
+    [8, 0, 2],
+    [1, 1, -2]
+]
+matrizB = [
+    [4, 2],
+    [4, 2],
+    [-4, -2]
+]
+
+def somarMatriz():
+
+    for i in range(3):
+        for j in range(3):
+            matrizA[0] + matrizB[0]
+
+
+
